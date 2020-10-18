b3c098a796f73cf960645183c81587def3904bb9
Maksudbek-I <m.igamberdyev@yandex.ru>
2020-10-18 23:11:49 +0300

    Initial commit

--- src/Practice/Practice_12/Main.java
+++ src/Practice/Practice_12/Main.java
@@ -6,6 +6,7 @@ public class Main {
         colorCaption("— С макарошками?", Colors.RED);
         colorCaption("—Нет, с пюрешкой!", Colors.YELLOW);
     }
+
     public static void colorCaption(String phrase, Colors color) {
         System.out.println(color.getColorCode() + phrase + "\u001B[0m");
     }
